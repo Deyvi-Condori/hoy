9ffda45a07187a2781ec6b10f6ca0ec73b6c56f0
ESTUDIANTE <ESTUDIANTE@ULELAB225>
2023-09-23 13:00:29 -0500

    Agregar archivos de proyecto.

---
+++ ClasePila.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hoy
+{
+    //
+    //
+    public class ClasePila
+    {
+        //
+        private readonly int Max;
+        private int Top;
+        private int[] Arreglo;
+
+        //Constructor
+        public ClasePila(int Tamaño)
+        {
+            Top = 0;
+            Max = Tamaño;
+            Arreglo = new int[Tamaño];
+        }
+        //
+        private bool EstaLlena()
+        {
+            if(Top==Max)
+                return true;
+            else 
+                return false;
+        }
+        //
+        private bool EstaVacia()
+        {
+            if(Top==0)
+                return true;
+            else
+                return false;
+        }
+        //
+        public bool Push(int dato)
+        {
+            if (!EstaLlena())
+            {
+                Arreglo[Top] = dato;
+                Top++;
+                return true;
+            }
+            else 
+                return false;
+        }
+        //
+        public bool Pop() 
+        {
+            if (!EstaVacia())
+            {
+                Top --;
+                return true;
+            }
+            else
+                return false;
+        }
+        //
+        public string Mostrar()
+        {
+            string Resultado = "\n";
+            if (!EstaVacia()) 
+            {
+                //
+                for(int i = 0; i<=Top -1 ; i++)
+                    Resultado = Resultado + "\n[" +i.ToString() +"] ->" + Arreglo[i].ToString();
+            }
+            else
+                Resultado = "\n Cola Vacia";
+
+            Resultado = Resultado + "\n\nTop = " + Top.ToString();
+            Resultado = Resultado + "\n\nMax = " + Max.ToString();
+            return (Resultado);
+        }
+        public void Vaciar()
+        {
+            
+            Top = -1;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hoy
+{
+    internal class Program
+    {
+        //
+        static ClasePila Pila = new ClasePila(10);
+        static void Main(string[] args)
+        {
+            int opcion = 0;
+            do 
+            {
+                Console.Clear();
+                Console.WriteLine("Pilas - Arreglos");
+                Console.WriteLine("******************");
+                Console.WriteLine("\n1. Insertar Dato (Push)");
+                Console.WriteLine("*2. Eliminar Dato(Pop)*");
+                Console.WriteLine("*3. Mostrar Dato en Pila*");
+                Console.WriteLine("*4. Eliminar Dato de la Pila*");
+                Console.WriteLine("0. Salir");
+                Console.Write("\n \nOpcion : ");
+                opcion=int.Parse(Console.ReadLine());
+                switch(opcion) 
+                {
+                    case 1: InsertarEnPila(); break; 
+                    case 2: EliminarEnPilas(); break;
+                    case 3: MostrarDatos(); break;
+                    case 4: VaciarPilas(); break;
+                }
+            }while(opcion!=0);
+        }
+        //
+        public static void InsertarEnPila()
+        {
+            int Dato = 0;
+            Console.Clear();
+            Console.WriteLine("Insertar dato en Pila");
+            Console.Write("Numero entero : ");
+            Dato = int.Parse(Console.ReadLine());
+            //
+            if (Pila.Push(Dato))
+                Console.WriteLine("Dato insertado en la Pila...!");
+            else
+                Console.WriteLine("\nPila llena...!");
+            Console.ReadKey();
+        }
+        //
+        public static void EliminarEnPilas() 
+        {
+            Console.Clear();
+            Console.WriteLine("Eliminar dato en Pila");
+            Console.Write("Numero entero : ");
+            //
+            if (Pila.Pop())
+                Console.WriteLine("Dato eliminado en la Pila...!");
+            else
+                Console.WriteLine("\nPila vacia...!");
+            Console.ReadKey();
+        }
+        // 
+        public static void MostrarDatos()
+        {
+            Console.Clear();
+            Console.WriteLine("Datos almacenados en la Pila");
+            //
+            Console.WriteLine(Pila.Mostrar());
+            Console.ReadKey();
+        }
+        //
+        public static void VaciarPilas() 
+        {
+            char sn;
+            Console.Clear();
+            Console.WriteLine("Eliminar la Pila");
+            //
+            do 
+            {
+                Console.Write("Esta seguro de desear vaciar la Pila");
+                Console.WriteLine(" ");
+                sn = char.Parse(Console.ReadLine());
+                sn = Char.ToUpper(sn);
+
+
+            }while(sn!= 'S' && sn!= 'N');
+            if (sn == 'S')
+            {
+                Console.WriteLine();
+                Console.ReadKey();
+            }
+        }
+    }
+}
